82ec1b6412466963115902f3b7bfaf87ed352256
nanduvinayak <112405053+nanduvinayak@users.noreply.github.com>
2023-12-08 23:37:52 -0500

    footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
-<!-- Your Global Footer Content Here -->
+
 <footer>
     <div >
-        <!-- Your footer content goes here -->
+        
         &copy; <?php echo date("Y"); ?> Your Website Name. All Rights Reserved.
     </div>
 </footer>
